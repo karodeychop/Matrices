380f97600c93f0e13634d4edc30aa6652b99b076
karodeychop <117703281+karodeychop@users.noreply.github.com>
2023-01-12 22:20:12 -0600

    Add tests
    Added tests for new identity matrix constructor functionality as well as testing for a matrix with populated data to ensure populateDataField function still works correctly.

--- src/main/java/barnes/jacob/matrices/Test.java
+++ src/main/java/barnes/jacob/matrices/Test.java
@@ -3,15 +3,20 @@ package barnes.jacob.matrices;
 public class Test {
 
     public static void main(String[] args){
-        Integer[][] identityData = {{1, 0}, {0, 1}};
 
-        Matrix identityTest = new Matrix(identityData);
+        Integer[][] populatedData = {{1, 2, 3}, {4, 5, 6}, {7, 8 ,9}};
+        Matrix identityTest = new Matrix(8);
         Matrix emptyMatrixTest = new Matrix(20, 3);
+
+        Matrix populatedMatrixTest = new Matrix(populatedData);
 
         System.out.println("Identity Test:");
         System.out.println(identityTest);
 
         System.out.println("Empty Matrix Test:");
         System.out.println(emptyMatrixTest);
+
+        System.out.println("Populated Matrix Test:");
+        System.out.println(populatedMatrixTest);
     }
 }
